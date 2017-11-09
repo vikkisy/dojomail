0b002d5654649b8dfbc1af0b085adff38780c7e5
Vickie <vsysoup@gmail.com>
2017-11-09 13:01:40 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+ 
+  importance: boolean;
+  users = [
+    {
+      email: 'bill@gates.com', 
+      importance:true, 
+      subject:"new windows", 
+      content:"windows XI will launch in year 2100"
+    },
+    {
+      email: 'ada@lovelace.com',
+      importance: true,
+      subject: "programming",
+      content: "enchantress of numbers"
+    },
+    {
+      email: 'john@carmac.com',
+      importance:false,
+      subject: "updated algo",
+      content: "new algorithm for shadow volumes"
+    },
+    
+    {
+      email: 'gabe@newel.com',
+      importance:false,
+      subject: "hl3",
+      content: "just kidding..."
+    },
+  
+]
+
+
 }
